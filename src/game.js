975572d71e2feb1360414fcf66564d5191c136c0
William Phelps <williamphelps13@gmail.com>
2021-06-12 10:19:06 -0600

    Convert emoji to image assets path and add to pseudocode for Check for Win

--- src/game.js
+++ src/game.js
@@ -1,20 +1,35 @@
 class Game {
   constructor() {
     this.board = {playerOne:[], playerTwo: [],};
-    this.playerOne = new Player('one', '🐶', 0);
-    this.playerTwo = new Player('two', '🐱', 0);
+    this.playerOne = new Player('one', 'assets/Dog_Emoji_large.png', 0);
+    this.playerTwo = new Player('two', 'assets/Happy_cat_emoji.png', 0);
   }
 }
 
 // GOAL:
 // - Make game fully playable without the DOM
 // - Do updating of the Game.board data, etc, from your console
-//
+
+
 // "Fully Playable":
-// - var playerOne = new Player('one', '🐶', 0)
-// - var playerTwo = new Player('two', '🐱', 0)
-// - Record of Who's Turn it Currently Is
+// - this.board
 // - Record of Game Board Data (where are pieces)
+
+
+// - this.playerOne = new Player('one', '🐶', 0)
+// - this.playerTwo = new Player('two', '🐱', 0)
+
+// - Record of Who's Turn it Currently Is
+
 // - Check Game Board Date for Win
+// - 1,2,3 
+// - 4,5,6
+// - 7,8,9
+// - 1,4,7
+// - 2,5,8
+// - 3,6,9
+// - 1,5,9
+// - 3,5,7
 // - Check Game Board Data for Draw
+
 // - Board Reset
