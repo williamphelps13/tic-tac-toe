f8528951665741ec17d039ebbed0922db0de1dc3
William Phelps <williamphelps13@gmail.com>
2021-06-12 20:59:29 -0600

    Add querySelectors

---
+++ src/main.js
@@ -0,0 +1,14 @@
+// querySelectors
+var headerPlayer1Wins = document.querySelector("#headerPlayer1Wins");
+var imageTurns = document.querySelector("#imageTurns");
+var gameBoard = document.querySelector("#gameBoard");
+var square1 = document.querySelector("#square1");
+var square2 = document.querySelector("#square2");
+var square3 = document.querySelector("#square3");
+var square4 = document.querySelector("#square4");
+var square5 = document.querySelector("#square5");
+var square6 = document.querySelector("#square6");
+var square7 = document.querySelector("#square7");
+var square8 = document.querySelector("#square8");
+var square9 = document.querySelector("#square9");
+var headerPlayer2Wins = document.querySelector("#headerPlayer2Wins");
