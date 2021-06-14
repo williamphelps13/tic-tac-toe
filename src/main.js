2a2ab996809c2a36358b7b9d09a370dc67bc421e
William Phelps <williamphelps13@gmail.com>
2021-06-14 13:22:40 -0600

    Refactor DOM to DRY out function

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ var square6 = document.getElementById('6');
 var square7 = document.getElementById('7');
 var square8 = document.getElementById('8');
 var square9 = document.getElementById('9');
-var allSquares = document.querySelectorAll('button');
+// var allSquares = document.querySelectorAll('button');
 var headerPlayer2Wins = document.getElementById('headerPlayer2Wins');
 
 // Global Variables
@@ -29,8 +29,7 @@ function getWins() {
  addWinsToHeader()
 }
 
-function placeToken(event) {
-  event.preventDefault(event);
+function placeToken() {
   var dogTurnHeader = '<h2>It\'s</h2> <span> <img id="imageTurns" class="player-turn-token" src="assets/Dog_Emoji_large.png" alt="Turn toggle icon"> </span> <h2>\'s turn</h2>';
   var catTurnHeader = '<h2>It\'s</h2> <span> <img id="imageTurns" class="player-turn-token" src="assets/Happy_cat_emoji.png" alt="Turn toggle icon"> </span> <h2>\'s turn</h2>';
   var clickedSquare = event.target.closest('button');
@@ -67,33 +66,22 @@ function updateOnWin(dogTurnHeader, catTurnHeader) {
     currentGame.currentWinner = null;
     turnHeader.innerHTML = '<img class="player-won" src="assets/Dog_Emoji_large.png" alt="Dog token for player one"><h2>won!</h2>';
     gameBoard.removeEventListener('click', placeToken)
-    setTimeout(function () {
-      clearSquares()
-      if (currentGame.turn % 2 === 0) {
-        turnHeader.innerHTML = dogTurnHeader;
-        gameBoard.addEventListener('click', placeToken);
-      } else {
-        turnHeader.innerHTML = catTurnHeader;
-        gameBoard.addEventListener('click', placeToken);
-    }}, 1000);
+    delayDisplay(dogTurnHeader, catTurnHeader)
   } else if (currentGame.currentWinner === "two") {
     currentGame.currentWinner = null;
     turnHeader.innerHTML = '<img class="player-won" src="assets/Happy_cat_emoji.png" alt="Cat token for player two"><h2> won!</h2>';
     gameBoard.removeEventListener('click', placeToken)
-    setTimeout(function () {
-      clearSquares()
-      if (currentGame.turn % 2 === 0) {
-        turnHeader.innerHTML = dogTurnHeader;
-        gameBoard.addEventListener('click', placeToken);
-      } else {
-        turnHeader.innerHTML = catTurnHeader;
-        gameBoard.addEventListener('click', placeToken);
-    }}, 1000);
+    delayDisplay(dogTurnHeader, catTurnHeader)
   } else if (currentGame.currentWinner === "draw") {
     currentGame.currentWinner = null;
     turnHeader.innerHTML = '<h2 class="player-draw">It\'s a draw!</h2>';
     gameBoard.removeEventListener('click', placeToken)
-    setTimeout(function () {
+    delayDisplay(dogTurnHeader, catTurnHeader)
+  }
+};
+
+function delayDisplay(dogTurnHeader, catTurnHeader) {
+  setTimeout(function () {
     clearSquares()
     if (currentGame.turn % 2 === 0) {
       turnHeader.innerHTML = dogTurnHeader;
@@ -101,44 +89,26 @@ function updateOnWin(dogTurnHeader, catTurnHeader) {
     } else {
       turnHeader.innerHTML = catTurnHeader;
       gameBoard.addEventListener('click', placeToken);
-    }}, 1000);
+  }}, 1500);
+}
+
+function clearSquares() {
+  for (var i = 1; i <= 9; i++) {
+    var squareNum = "square" + i;
+    window[squareNum].innerHTML = '';
   }
 };
 
-function clearSquares() {
-  square1.innerHTML = "";
-  square2.innerHTML = "";
-  square3.innerHTML = "";
-  square4.innerHTML = "";
-  square5.innerHTML = "";
-  square6.innerHTML = "";
-  square7.innerHTML = "";
-  square8.innerHTML = "";
-  square9.innerHTML = "";
-}
-
 function renderBoard(playerBoard, token) {
-  if (playerBoard.includes(1)) {
-    square1.innerHTML = token; }
-  if (playerBoard.includes(2)) {
-    square2.innerHTML = token; }
-  if (playerBoard.includes(3)) {
-    square3.innerHTML = token; }
-  if (playerBoard.includes(4)) {
-    square4.innerHTML = token; }
-  if (playerBoard.includes(5)) {
-    square5.innerHTML = token; }
-  if (playerBoard.includes(6)) {
-    square6.innerHTML = token; }
-  if (playerBoard.includes(7)) {
-    square7.innerHTML = token; }
-  if (playerBoard.includes(8)) {
-    square8.innerHTML = token; }
-  if (playerBoard.includes(9)) {
-    square9.innerHTML = token; }
+  for (var i = 1; i <= 9; i++) {
+    var squareNum = "square" + i;
+    if (playerBoard.includes(i)) {
+      window[squareNum].innerHTML = token;
+    }
+  }
 };
 
 function addWinsToHeader() {
   headerPlayer1Wins.innerText = `${currentGame.playerOne.wins} wins`;
   headerPlayer2Wins.innerText = `${currentGame.playerTwo.wins} wins`;
-}
+};
