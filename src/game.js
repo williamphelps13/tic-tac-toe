aa9700f97b3c4d3111903882ae7c88915a313ab6
William Phelps <williamphelps13@gmail.com>
2021-06-15 14:54:33 -0600

    Change double quotation to single quotation for consistency

--- src/game.js
+++ src/game.js
@@ -6,8 +6,8 @@ class Game {
     };
     this.turn = 0;
     this.currentWinner = null;
-    this.playerOne = new Player("one", 'assets/Dog_Emoji_large.png', 0);
-    this.playerTwo = new Player("two", 'assets/Happy_cat_emoji.png', 0);
+    this.playerOne = new Player('one', 'assets/Dog_Emoji_large.png', 0);
+    this.playerTwo = new Player('two', 'assets/Happy_cat_emoji.png', 0);
   }
 
 
@@ -18,6 +18,7 @@ class Game {
 
   checkForWin(playerBoard, player) {
     var possibleWins = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
+
     for (var i = 0; i < possibleWins.length; i++) {
       if (possibleWins[i].every(num => playerBoard.includes(num))) {
         this.awardWinner(player);
@@ -29,9 +30,9 @@ class Game {
     this.addToWins(player);
 
     if (player === this.playerOne) {
-      this.currentWinner = "one";
+      this.currentWinner = 'one';
     } else if (player === this.playerTwo) {
-      this.currentWinner = "two";
+      this.currentWinner = 'two';
     }
 
     this.resetBoard();
@@ -43,7 +44,7 @@ class Game {
 
   checkForDraw() {
     if (this.board.one.length === 5 || this.board.two.length === 5) {
-      this.currentWinner = "draw";
+      this.currentWinner = 'draw';
       this.resetBoard();
     }
   }
